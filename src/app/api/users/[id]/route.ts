3559121fb2c0df907c95541c13819065d0c2946d
riadkhan60 <khanriad60@gmail.com>
2025-03-06 04:53:54 +0600

    idk

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 export async function GET(
-  request: NextRequest,
+  request: Request,
   context: { params: { id: string } },
 ) {
   const { id } = context.params;
