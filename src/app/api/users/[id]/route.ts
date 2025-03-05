808d01480dad1ac6d943410c5408b938d17ef439
riadkhan60 <khanriad60@gmail.com>
2025-03-06 04:33:22 +0600

    fix again

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,7 +1,8 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 export async function GET(
+  request: NextRequest,
   { params }: { params: { id: string } },
 ) {
   try {
