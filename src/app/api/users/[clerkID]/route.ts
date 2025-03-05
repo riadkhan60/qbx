de4c0aa03a7be23f88dd9f9d2c186bd87b24dcc6
riadkhan60 <khanriad60@gmail.com>
2025-03-06 05:16:40 +0600

    changed

--- src/app/api/users/[clerkID]/route.ts
+++ src/app/api/users/[clerkID]/route.ts
@@ -1,26 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { clerkId: string } },
-) {
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        clerkID: params.clerkId, 
-      },
-    });
 
-    if (!user) {
-      return NextResponse.json({ error: 'User not found' }, { status: 404 });
-    }
-
-    return NextResponse.json(user, { status: 200 });
-  } catch {
-    return NextResponse.json({ error: 'Error fetching user' }, { status: 500 });
-  }
-}
 
 
 // PUT /api/users/[clerkId] - Update a user
